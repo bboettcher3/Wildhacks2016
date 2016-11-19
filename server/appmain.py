f4a7043b9eb07bff3b5d92bf47cc148c7d521859
George <georgealeks@hotmail.com>
2016-11-19 00:29:47 -0600

    Basic server stuff

---
+++ server/appmain.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+@app.route("/index.html")
+@app.route("/index")
+@app.route("/")
+def main():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run()
